2200778f51cb67a02495e434affdb1de74cfdf2a
jiangxiaowu <jiangxiaowu6868@gmail.com>
2020-10-13 10:02:23 +0800

    login

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-content>
+      <!-- <router-link to="/login">Login</router-link> -->
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  components: {
+ 
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/login/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+      <h1>list</h1>
+  </v-app>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
