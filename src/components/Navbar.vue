ce912eff8817ebaf29bfb6bd5d9f5dbedde7733f
jiangxiaowu <jiangxiaowu6868@gmail.com>
2020-10-13 11:22:00 +0800

    page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,24 @@
 <template>
   <nav>
-      <v-navigation-drawer v-model="drawer" dark app mini-variant class="pink accent-2">
-          
-      </v-navigation-drawer>
+    <v-navigation-drawer
+      v-model="drawer"
+      dark
+      app
+      mini-variant
+      class="pink accent-2"
+    >
+      <v-list tile>
+        <v-list-item class="mb-8" router to="/">
+          <v-list-item-action> </v-list-item-action>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
   </nav>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
